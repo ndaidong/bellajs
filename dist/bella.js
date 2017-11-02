0ed649403c626bbc8d007fbf2ce6d1684a91311a
Dong Nguyen <ndaidong@gmail.com>
2017-11-02 17:48:10 +0700

    v7.1.3
    - Update md5 so that Rollup no longer include it when not using

--- dist/bella.js
+++ dist/bella.js
@@ -1,6 +1,6 @@
 /**
- * bellajs@7.1.2
- * built on: Mon, 07 Aug 2017 06:41:40 GMT
+ * bellajs@7.1.3
+ * built on: Thu, 02 Nov 2017 10:47:26 GMT
  * repository: https://github.com/ndaidong/bellajs
  * maintainer: @ndaidong
  * License: MIT
@@ -10,25 +10,132 @@
   typeof define === 'function' && define.amd ? define(['exports'], factory) :
   (factory((global.bella = {})));
 }(this, (function (exports) { 'use strict';
-  var md5 = function () {
-    for (var m = [], l = 0; 64 > l;) {
-      m[l] = 0 | 4294967296 * Math.abs(Math.sin(++l));
-    }return function (c) {
-      var e,
-          g,
-          f,
-          a,
-          h = [];c = unescape(encodeURI(c));for (var b = c.length, k = [e = 1732584193, g = -271733879, ~e, ~g], d = 0; d <= b;) {
-        h[d >> 2] |= (c.charCodeAt(d) || 128) << 8 * (d++ % 4);
-      }h[c = 16 * (b + 8 >> 6) + 14] = 8 * b;for (d = 0; d < c; d += 16) {
-        b = k;for (a = 0; 64 > a;) {
-          b = [f = b[3], (e = b[1] | 0) + ((f = b[0] + [e & (g = b[2]) | ~e & f, f & e | ~f & g, e ^ g ^ f, g ^ (e | ~f)][b = a >> 4] + (m[a] + (h[[a, 5 * a + 1, 3 * a + 5, 7 * a][b] % 16 + d] | 0))) << (b = [7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21][4 * b + a++ % 4]) | f >>> 32 - b), e, g];
-        }for (a = 4; a;) {
-          k[--a] = k[a] + b[a];
+  var md5 = function md5(str) {
+    var k = [],
+        i = 0;
+    for (; i < 64;) {
+      k[i] = 0 | Math.abs(Math.sin(++i)) * 4294967296;
+    }
+    var b,
+        c,
+        d,
+        j,
+        x = [],
+        str2 = unescape(encodeURI(str)),
+        a = str2.length,
+        h = [b = 1732584193, c = -271733879, ~b, ~c],
+        i = 0;
+    for (; i <= a;) {
+      x[i >> 2] |= (str2.charCodeAt(i) || 128) << 8 * (i++ % 4);
+    }x[str = (a + 8 >> 6) * 16 + 14] = a * 8;
+    i = 0;
+    for (; i < str; i += 16) {
+      a = h;j = 0;
+      for (; j < 64;) {
+        a = [d = a[3], (b = a[1] | 0) + ((d = a[0] + [b & (c = a[2]) | ~b & d, d & b | ~d & c, b ^ c ^ d, c ^ (b | ~d)][a = j >> 4] + (k[j] + (x[[j, 5 * j + 1, 3 * j + 5, 7 * j][a] % 16 + i] | 0))) << (a = [7, 12, 17, 22, 5, 9, 14, 20, 4, 11, 16, 23, 6, 10, 15, 21][4 * a + j++ % 4]) | d >>> 32 - a), b, c];
+      }
+      for (j = 4; j;) {
+        h[--j] = h[j] + a[j];
+      }
+    }
+    str = '';
+    for (; j < 32;) {
+      str += (h[j >> 3] >> (1 ^ j++ & 7) * 4 & 15).toString(16);
+    }return str;
+  };
+  var asyncGenerator = function () {
+    function AwaitValue(value) {
+      this.value = value;
+    }
+    function AsyncGenerator(gen) {
+      var front, back;
+      function send(key, arg) {
+        return new Promise(function (resolve, reject) {
+          var request = {
+            key: key,
+            arg: arg,
+            resolve: resolve,
+            reject: reject,
+            next: null
+          };
+          if (back) {
+            back = back.next = request;
+          } else {
+            front = back = request;
+            resume(key, arg);
+          }
+        });
+      }
+      function resume(key, arg) {
+        try {
+          var result = gen[key](arg);
+          var value = result.value;
+          if (value instanceof AwaitValue) {
+            Promise.resolve(value.value).then(function (arg) {
+              resume("next", arg);
+            }, function (arg) {
+              resume("throw", arg);
+            });
+          } else {
+            settle(result.done ? "return" : "normal", result.value);
+          }
+        } catch (err) {
+          settle("throw", err);
         }
-      }for (c = ""; 32 > a;) {
-        c += (k[a >> 3] >> 4 * (1 ^ a++ & 7) & 15).toString(16);
-      }return c;
+      }
+      function settle(type, value) {
+        switch (type) {
+          case "return":
+            front.resolve({
+              value: value,
+              done: true
+            });
+            break;
+          case "throw":
+            front.reject(value);
+            break;
+          default:
+            front.resolve({
+              value: value,
+              done: false
+            });
+            break;
+        }
+        front = front.next;
+        if (front) {
+          resume(front.key, front.arg);
+        } else {
+          back = null;
+        }
+      }
+      this._invoke = send;
+      if (typeof gen.return !== "function") {
+        this.return = undefined;
+      }
+    }
+    if (typeof Symbol === "function" && Symbol.asyncIterator) {
+      AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+        return this;
+      };
+    }
+    AsyncGenerator.prototype.next = function (arg) {
+      return this._invoke("next", arg);
+    };
+    AsyncGenerator.prototype.throw = function (arg) {
+      return this._invoke("throw", arg);
+    };
+    AsyncGenerator.prototype.return = function (arg) {
+      return this._invoke("return", arg);
+    };
+    return {
+      wrap: function (fn) {
+        return function () {
+          return new AsyncGenerator(fn.apply(this, arguments));
+        };
+      },
+      await: function (value) {
+        return new AwaitValue(value);
+      }
     };
   }();
   var toConsumableArray = function (arr) {
